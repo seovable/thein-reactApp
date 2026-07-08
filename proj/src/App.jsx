9b7d5ee24c5c7c974a0e2dd890a2f0fb68d2632a
seovable <lamie0814@naver.com>
2026-07-08 11:14:05 +0900

    initial commit

---
+++ proj/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './components/Header';
+import './index.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <h1
+        style={{
+          textAlign: 'center',
+          marginTop: '60px',
+        }}
+      >
+        ⚾ BaseMate에 오신 것을 환영합니다!
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ proj/src/components/header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header className="header">
+      <h2>⚾ BaseMate</h2>
+
+      <nav>
+        <a href="#">홈</a>
+        <a href="#">직관모집</a>
+        <a href="#">경기일정</a>
+        <a href="#">마이페이지</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
